73b24033d8bcdfd3c3d75f2ba752eb62d5286d89
hulub <hulub_sky_master@yahoo.com>
2019-12-02 14:19:00 +0100

    Moved generate_n and group_public_keys to aion_cli

--- README.md
+++ README.md
@@ -7,31 +7,11 @@ AVX Credential Generator is a tool for generation login credentials for Assembly
 $ bundle install
 ```
 
-## Use cases
-1. Generate n credential pairs, which consist of an election code and a public key. The credential pairs will be printed 
-in the terminal. The following command takes as arguments:
-   - n, the number of credential pairs
-   ```
-   $ bundler exec bin/avx_credential_generator generate n
-   ```
-    
-2. Reads a csv file and generate credential pairs for each entry of the file. Generates two new files with the initial
+## Use case
+Reads a csv file and generate credential pairs for each entry of the file. Generates two new files with the initial
 content plus an extra column for election code or public key, respectively. The outputted files are located in /outputs.
 The following command takes as arguments:
     - the path to the csv file
    ```
    $ bundler exec bin/avx_credential_generator generate_in_file file_path
-   ```
-
-3. Combine multiple public key files into one main public key file. The input files are the ones received from each
-credential authority. The output public key file is the one that needs to be imported into the AVX system. The outputted
-file is located in /outputs.
-The script has an interactive behaviour and the user needs to specify the column used as the voter identifier. The user
-also needs to specify the name of the output file.
-The script expects that all input files have the same data structure (csv files have the same columns) and that the data
-from all files is consistent (the identifier column is identical in all files).
-The following command takes as arguments:
-   - a list of all the paths to the public key files, separated by space
-   ```
-   $ bundler exec bin/avx_credential_generator group_public_key_files file1_path file2_path file3_path
    ```
--- lib/avx_credential_generator.rb
+++ lib/avx_credential_generator.rb
@@ -6,20 +6,6 @@ module AVXCredentialGenerator
   class Main < Thor
     include AVXCredentialGenerator::ApplicationHelper
 
-    desc 'say_hello', 'Prints hello world'
-    def say_hello
-      puts 'hello world !'
-    end
-
-    desc 'generate n', 'Generate n pair of election codes - public keys'
-    def generate(n)
-      credential_pairs = generate_credential_pairs(n.to_i)
-
-      credential_pairs.each do |election_code, public_key|
-        $stdout.puts "#{election_code}\t#{public_key}"
-      end
-    end
-
     desc 'generate_in_file PATH', 'Generate election codes for each identifier form the file'
     def generate_in_file(path)
       headers, *rows = read_csv(File.read(path))
@@ -43,52 +29,6 @@ module AVXCredentialGenerator
       $stdout.puts 'Outputted files:'
       $stdout.puts ec_absolute_path
       $stdout.puts pk_absolute_path
-    end
-
-    desc 'group_public_key_files FILE_PATHS', 'Group all public key files into one'
-    def group_public_key_files(*paths)
-      public_keys = {}
-
-      paths_first, *paths_rest = paths
-      file_headers, *file_rows = read_csv(File.read(paths_first))
-
-      index = ask_identifier_header_from_file(file_headers)
-
-      file_rows.each do |row|
-        id = row[index]
-        public_keys[id] = row.last
-      end
-
-
-      paths_rest.each do |path|
-        headers, *rows = read_csv(File.read(path))
-
-        rows.each do |row|
-          id = row[index]
-          public_key = row.last
-
-          unless public_keys.key?(id)
-            raise Thor::Error, 'Files are not consistent. Identifier column does not match across files'
-          end
-
-          public_keys[id] = Crypto.combine_public_keys(public_keys[id], public_key)
-        end
-      end
-
-
-
-      file_rows.zip(public_keys.values).map do |row, public_key|
-        # replace the old public key with the new computed one
-        row[row.size - 1] = public_key
-      end
-
-
-      # write to file
-      public_keys_file = ask_public_keys_path
-      write_csv(public_keys_file, file_headers, file_rows)
-
-      $stdout.puts 'Outputted files:'
-      $stdout.puts public_keys_file
     end
 
 
